144528da3906433e76956471202b53c562000df2
Ali Bouhcain <alibouhcain@gmail.com>
2023-01-16 18:43:11 +0100

    Add Authentification

--- TechShop/Models/MockCategoryRepository.cs
+++ TechShop/Models/MockCategoryRepository.cs
@@ -5,8 +5,8 @@
         public IEnumerable<Category> AllCategories =>
             new List<Category>
             {
-                new Category{CategoryId=1, CategoryName="Fruit Products", Description="All-fruity Products"},
-                new Category{CategoryId=2, CategoryName="Cheese cakes", Description="Cheesy all the way"},
+                new Category{CategoryId=1, CategoryName="Smart Phones", Description="All-fruity Products"},
+                new Category{CategoryId=2, CategoryName="Laptops", Description="Cheesy all the way"},
                 new Category{CategoryId=3, CategoryName="Seasonal Products", Description="Get in the mood for a seasonal Product"}
             };
     }
